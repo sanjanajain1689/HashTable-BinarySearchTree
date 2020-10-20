69c51ce1766c5afa522a8bf3820565888362b017
Sanjana Jain <sanjana.jain04@gmail.com>
2020-10-20 07:27:32 +0530

    Added UC3 BST Recursive Search

--- test/java/BinaryTreeTest.java
+++ test/java/BinaryTreeTest.java
@@ -13,4 +13,24 @@ public class BinaryTreeTest {
                 myBinaryTree.getRoot().right.key.equals(70);
         Assert.assertTrue(result);
     }
+
+    @Test
+    public void givenANumber_WhenExistsInBinaryTree_ShouldReturnTrue() {
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(60);
+        myBinaryTree.add(95);
+        myBinaryTree.add(11);
+        myBinaryTree.add(65);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
+        boolean result = myBinaryTree.search(63);
+        Assert.assertTrue(result);
+    }
 }
